3856582f2938b659a50b52a40947d7d49aba43c8
hiro-su <h.sugipon@gmail.com>
2013-03-25 01:27:51 +0900

    edit: test/test_sleep.rb

--- test/test_run.rb
+++ test/test_run.rb
@@ -1,13 +1,13 @@
 require File.expand_path(File.dirname(__FILE__)) + "/helper.rb"
 
 class PollingTest < Test::Unit::TestCase
-  def test_run
-    puts "start: #{Time.now}"
-    Polling.setting offset: 30, debug: true
-    Polling::run(5) do
-      puts Time.now
-      puts "exec sleep 2"
-      sleep 2
-    end
-  end
+  #def test_run
+  #  puts "start: #{Time.now}"
+  #  Polling.setting offset: 30, debug: true
+  #  Polling::run(5) do
+  #    puts Time.now
+  #    puts "exec sleep 2"
+  #    sleep 2
+  #  end
+  #end
 end
--- test/test_sleep.rb
+++ test/test_sleep.rb
@@ -2,8 +2,8 @@ require File.expand_path(File.dirname(__FILE__)) + "/helper.rb"
 
 class PollingTest < Test::Unit::TestCase
   def test_sleep
-    assert_equal false, Polling::Sleep::exec(-1)
-    assert_equal false, Polling::Sleep::exec(0)
-    #assert_equal 5, Polling::Sleep::exec(5)
+    assert_equal false, Polling::Sleep.exec(-1)
+    assert_equal false, Polling::Sleep.exec(0)
+    assert_equal 1, Polling::Sleep.exec(1)
   end
 end
